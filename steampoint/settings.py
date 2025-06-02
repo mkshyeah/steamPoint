a00bbdb60dea4a0fa62986451182ecbab5b11b07
Вичкуткин Максим <maks.vichkutkin98@gmail.com>
2025-06-02 19:42:04 +0500

    add registration app

--- steampoint/settings.py
+++ steampoint/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'booking.apps.BookingConfig',
     'users.apps.UsersConfig',
+    'registration.apps.RegistrationConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'steampoint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'steampoint',
+        'USER':'steampoint',
+        'PASSWORD':'steampoint',
+        'HOST':'localhost',
+        "PORT":'5432',
     }
 }
 
